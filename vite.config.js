d256698eefddf12e0e20a3af7058d97de9bd4597
Sandile <stl9970080@gmail.com>
2025-06-03 23:15:57 +0200

    commiting the 'dist' folder

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
-})
+  plugins: [react(), tailwindcss()],
+  base: '/Water-Management-System-for-SRM11A1/', // Replace with your repository name
+});
