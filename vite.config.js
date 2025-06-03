f856482cfae59c11c9da193a4b7ea0c907f699d5
Sandile <stl9970080@gmail.com>
2025-06-03 23:29:47 +0200

    Changing the repository name in 'base'

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/Water-Management-System-for-SRM11A1/', // Replace with your repository name
+  base: '/SRM_Webapp/', // Replace with your repository name
 });
